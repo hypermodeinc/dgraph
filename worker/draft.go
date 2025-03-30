0a9074afc33b9c4e5b50afb50753f14b19600f3d
Harshil Goel <harshil@hypermode.com>
2025-03-31 01:25:52 +0530

    changed sorting

--- worker/draft.go
+++ worker/draft.go
@@ -1016,14 +1016,23 @@ func (n *node) applyMutations(ctx context.Context, proposal *pb.Proposal) (rerr 
 	// It is possible that the user gives us multiple versions of the same edge, one with no facets
 	// and another with facets. In that case, use stable sort to maintain the ordering given to us
 	// by the user.
-	sort.SliceStable(m.Edges, func(i, j int) bool {
-		ei := m.Edges[i]
-		ej := m.Edges[j]
-		if ei.GetAttr() != ej.GetAttr() {
-			return ei.GetAttr() < ej.GetAttr()
-		}
-		return ei.GetEntity() < ej.GetEntity()
-	})
+	featureFlag := true
+	if featureFlag {
+		sort.SliceStable(m.Edges, func(i, j int) bool {
+			ei := m.Edges[i]
+			ej := m.Edges[j]
+			return ei.GetEntity() < ej.GetEntity()
+		})
+	} else {
+		sort.SliceStable(m.Edges, func(i, j int) bool {
+			ei := m.Edges[i]
+			ej := m.Edges[j]
+			if ei.GetAttr() != ej.GetAttr() {
+				return ei.GetAttr() < ej.GetAttr()
+			}
+			return ei.GetEntity() < ej.GetEntity()
+		})
+	}
 
 	txn := posting.Oracle().RegisterStartTs(m.StartTs)
 	if txn.ShouldAbort() {
@@ -1033,7 +1042,6 @@ func (n *node) applyMutations(ctx context.Context, proposal *pb.Proposal) (rerr 
 	// Discard the posting lists from cache to release memory at the end.
 	defer txn.Update()
 
-	featureFlag := true
 	if featureFlag {
 		mp := &MutationPipeline{txn: txn}
 		return mp.Process(ctx, m.Edges)
