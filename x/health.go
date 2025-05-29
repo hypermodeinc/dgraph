b1b96ead526fb8d85d88ad11414bbdf3894925dc
shivaji-dgraph <shivaji@dgraph.io>
2025-05-29 15:13:26 +0530

    fix trunk

--- x/health.go
+++ x/health.go
@@ -21,7 +21,6 @@ var (
 
 	healthCheck     uint32
 	errHealth       = errors.New("Please retry again, server is not ready to accept requests")
-	errImportMode   = errors.New("Please retry again, server is not ready to accept requests")
 	errDrainingMode = errors.New("the server is in draining mode " +
 		"and client requests will only be allowed after exiting the mode " +
 		" by sending a GraphQL draining(enable: false) mutation to /admin")
